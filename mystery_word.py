4a1ded0df67c1c027dc62863115f17d0d8bfaa30
chriscors <70537376+chriscors@users.noreply.github.com>
2023-02-16 18:07:08 -0500

    Adds random word selection

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,20 @@
-def play_game():
+def select_word(word_list: list):
+    import random
+    return (random.choice(word_list))
+
+
+def play_game(word: str):
     pass
 
 
 if __name__ == "__main__":
-    play_game()
+    from pathlib import Path
+    file = Path("test-word.txt")
+    with open(file) as opened_file:
+        parsed_file = opened_file.read()
+    print(type(parsed_file))
+    parsed_file = parsed_file.split('\n')
+
+    word = select_word(parsed_file)
+    print(word)
+    play_game(word)
